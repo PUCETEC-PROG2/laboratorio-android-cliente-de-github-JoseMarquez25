0b504a43ab975b829376e2c673649d9dd0b50e8b
José Miguel Márquez Figueroa <jmmarquezf@puce.edu.ec>
2025-11-14 14:21:55 -0500

    Implement View Binding and RecyclerView in MainActivity
    Refactor MainActivity to use View Binding and set up RecyclerView for displaying repositories.

--- app/src/main/java/ec/edu/uisek/githubclient/MainActivity.kt
+++ app/src/main/java/ec/edu/uisek/githubclient/MainActivity.kt
@@ -5,16 +5,31 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import ec.edu.uisek.githubclient.databinding.ActivityMainBinding
+import ec.edu.uisek.githubclient.models.Repo
+import ec.edu.uisek.githubclient.services.RetrofitClient
+import retrofit2.Callback
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var reposAdapter: ReposAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupRecyclerView()
+    }
+
+    private fun setupRecyclerView() {
+        reposAdapter = ReposAdapter()
+        binding.repoRecyclerView.adapter = reposAdapter
     }
 }
+
+    private fun fetchRepositories() {
+        val apiService = RetrofitClient.gitHubApiService
+        val call = apiService.getRepos()
+
+        call.enqueue(object : Callback<List<Repo>>)
+    }
